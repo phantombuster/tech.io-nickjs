5de1ae1a9113298dad7d5026039e8a136aa289d7
Antoine Gunzburger <antoine@phantombuster.com>
2017-06-30 15:40:52 +0200

    start nickjs

--- nodejs-project/src/nick.js
+++ nodejs-project/src/nick.js
@@ -1,17 +1,41 @@
-import 'babel-polyfill'
-import Nick from 'nickjs'
-import Promise from 'bluebird'
+import "babel-polyfill"
 
+import Nick from "nickjs"
 const nick = new Nick()
 
-nick.newTab().then(async function(tab) {
-	await tab.open('nickjs.org')
-	// ...
-	// Continue here
-	// ...
+// Simple scraping function, getting all the infos using jQuery and returning them with the callback "done"
+const scrape = (arg, done) => {
+    var persons = document.querySelectorAll(".person")
+    var result = []
+    for (i in persons) {
+        if (i >= 0 && i <= 100) {
+        	result.push({
+        	    name: persons[i].querySelector(".name.property-value").innerText,
+        	    birth_year: persons[i].querySelector(".birth_year.property-value").innerText,
+        	    death_year: persons[i].querySelector(".death_year.property-value").innerText,
+        	    gender: persons[i].querySelector(".gender.property-value").innerText
+        	})
+        }
+    }
+    done(null, result)
+}
+
+nick.newTab().then(async (tab) => {
+	// Open the webpage
+	await tab.open("http://scraping-challenges.phantombuster.com/onepage")
+	// Wait for the data to be visible
+	await tab.waitUntilVisible(".person")
+	// Launch the scrape function in the page context
+	const result = await tab.evaluate(scrape)
+	// Take a screenshot of the whole page
+	await tab.screenshot("screenshot.jpg")
+	// Send the data in the result object
+	console.log(JSON.stringify(result, null, 2))
 })
-.then(() => nick.exit())
+.then(() => {
+	nick.exit(0)
+})
 .catch((err) => {
-	console.log('Oops, an error occurred: ' + err)
+	console.log(`Something went wrong: ${err}`)
 	nick.exit(1)
 })
