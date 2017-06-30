be51041da269605555616bb1b2172c2cd5872fe7
Antoine Gunzburger <antoine@phantombuster.com>
2017-06-30 12:20:52 +0200

    start nickjs

--- markdowns/welcome.md
+++ markdowns/welcome.md
@@ -1,3 +1,3 @@
 # Run NickJS
 
-@[Test NickJS]({ "stubs": ["nick.js"], "command": "node_modules/mocha/bin/mocha nick.spec.js --reporter list" })
+@[Test NickJS]({ "stubs": ["nick.js"], "command": "npm run build && ./node_modules/casperjs/bin/casperjs lib/test.js" })
--- nodejs-project/nick.js
+++ nodejs-project/nick.js
@@ -1,7 +1,17 @@
-const launch = text => text;
+import 'babel-polyfill'
+import Nick from 'nickjs'
+import Promise from 'bluebird'
 
-// { autofold
-module.exports = {
-  launch: launch
-};
-// }
+const nick = new Nick()
+
+nick.newTab().then(async function(tab) {
+	await tab.open('nickjs.org')
+	// ...
+	// Continue here
+	// ...
+})
+.then(() => nick.exit())
+.catch((err) => {
+	console.log('Oops, an error occurred: ' + err)
+	nick.exit(1)
+})
